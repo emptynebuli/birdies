ee75058c9cc45bbefe20f29a9de28a136514ce29
burch <emptynebuli@local>
2025-02-06 09:56:17 -0500

    initial

---
+++ atredis-bird.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+const bird = `
+       .                                                               ..       
+    ..'''...                                                        ..'''''...  
+ .',,,;;;;,;,,'.                                               .';;;;;;;;;;,,'. 
+ ..,;::::::ccoO0ko:'..                                    .';lx0KOocc::::::;,'. 
+ ...',,;:loxO0XWMMWXKOkdl;.           .''.          .'coxOKXNWMMWNK0kdlc:;,,'.. 
+ ..'',,;cd0NWMMMMMMMMMMMMWXkl,.       cOOl.      .;o0NWMMMMMMMMMMMMWNKxc;;,,'.. 
+   .',:coONMMMMMMMMMMMMMMMMMWXOo:'.  .oKKd.  .,cx0NWMMMMMMMMMMMMMMMMMW0oc;,'.   
+   ..';cx0KKNWMMMMMMMMMMMMMMMMMWWXOdco0WWKdox0NWMMMMMMMMMMMMMMMMMMWX0OOxc,...   
+      .;;,..lKWMMMMMMMMMMMMMMMMMMMMMWWMMMMMMMMMMMMMMMMMMMMMMMMMMMWKc...,,.      
+            .:OXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXk;             
+              'l0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN0l.              
+               .,oOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNKkc'.               
+                  .,ckXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKx:..                  
+                     .':dOXNWMMMMMMMMMMMMMMMWMMMMMMWXKkl,.                      
+                         .';ldkOOOkONMMMMMMNkxkxxdo:'..                         
+                               ....'kWMMMMWk'                                   
+                                   'kWMMMMWO,                                   
+                                  'xNMMMMMMWO,                                  
+                                 'xNWMMMMMWWXk,                                 
+                                .;oxONWMMWNkdo;.                                
+                                 .',:k0KKOx:'.                                  
+                                     .;ll,.                                     
+                                       ..               
+`
+
+func main() {
+	fmt.Println(bird)
+}
